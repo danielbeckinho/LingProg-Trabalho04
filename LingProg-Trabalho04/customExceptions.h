0005ec54b76b669cc5978196d3d137f9b416d8c3
Daniel Becker <nonubs@gmail.com>
2020-11-19 13:20:11 -0300

    wip
    fixed extra qualifier error in customExceptions.h

--- LingProg-Trabalho04/customExceptions.h
+++ LingProg-Trabalho04/customExceptions.h
@@ -1,16 +1,20 @@
+#ifndef CUSTOMEXCEPTIONS_H
+#define CUSTOMEXCEPTIONS_H
+
 #include <stdexcept>
 //#include <exception>
 
 
 class ExceptionPatientAlreadyExists: public std::runtime_error {
     public:
-        ExceptionPatientAlreadyExists::ExceptionPatientAlreadyExists()
+        ExceptionPatientAlreadyExists()
         : runtime_error("\n Paciente ja existe. Cadastramento duplicado impedido. \n") {}
 };
 
 class ExceptionPatientNotFound: public std::runtime_error {
     public:
-        ExceptionPatientNotFound::ExceptionPatientNotFound()
+        ExceptionPatientNotFound()
         : runtime_error("\n Paciente nao encontrado. Tente novamente. \n") {}
 };
 
+#endif
